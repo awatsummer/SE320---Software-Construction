c6786e9a71c4891a8d4eb0239eb6cbe142a38989
awatsummer <71294412+awatsummer@users.noreply.github.com>
2022-10-09 17:56:49 -0400

    Assignment 2 - Question 1

--- Question1.java
+++ Question1.java
@@ -1,56 +1,133 @@
-import java.util.InputMismatchException;
+/**
+ * AnnaMaria Summer
+ * SE 320 - Software Construction
+ * Assignment 2 - Question 1
+ * Revise the GenericStack class above to implement it using an array rather than an ArrayList:
+ *  - You must have a constructor to construct a stack with the default initial capacity.
+ *  - You must have another constructor to construct a stack with a specified initial capacity.
+ *  - You should check the array size before adding a new element to the stack. If the array is
+ *    full, create a new array that doubles the current array size and copy the elements from
+ *    the current array to the new array.
+ */
+
 import java.util.Scanner;
 
-/**
-  	Author: AnnaMaria Summer
-	Date: 9/15/22
-	Assignment 1
-	Question 1
-	
-	1. Write a Java program that prompts the user to enter two integers 
-		and displays their sum. Use exception handling to prompt the user 
-		for reading the number again if the input is incorrect.	
-*/
+public class Question1 {
 
-public class Question1 {	
-	
-	public static void main(String[] args) {
-		
-		System.out.println("This program will take 2 integer inputs and display the sum\n");
-		
-		// Initializing scanner, the 2 numbers variables, and the boolean to determine if input is correct
-		Scanner s = new Scanner(System.in);
-		boolean valid = true;
-		int num1 = 0;
-		int num2 = 0;
-		
-		// Do-while loop to ensure program runs until valid numbers are entered (until valid=true)
-		do {
-			
-			try {
-				// Getting the 2 number inputs and if no exceptions are thrown, valid will be set to true
-				System.out.println("Enter 2 integers: ");
-				s = new Scanner(System.in);
-				num1 = s.nextInt();
-				num2 = s.nextInt();
-				valid = true;
-				
-			} catch(InputMismatchException e) {
-				// Catching exception if the input entered is not an integer and setting valid=false
-				System.out.println("Not an integer! ");
-				s.nextLine();
-				valid = false;
-			} 
-			
-		} while(valid == false);
-		
-		s.close();
-		
-		// Printing out the sum of the input numbers
-		System.out.println("Sum of the numbers: " + (num1 + num2));
-	}
-	
-	
-	
-	
+    public static void main(String[] args){
+        // Creating a test stack of the default size
+        GenericStack<String> testWithDefaultSize = new GenericStack<>();
+
+        // Getting user input for the size of an array
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size for the array: ");
+        int arraySize = sc.nextInt();
+        GenericStack<String> testWithChosenSize = new GenericStack<>(arraySize);
+        System.out.println("Stack Empty: " + testWithChosenSize.isEmpty());
+        testWithChosenSize.push("Hello");
+        testWithChosenSize.push("There");
+        testWithChosenSize.push("General");
+        testWithChosenSize.push("Kenobi");
+
+        System.out.println(testWithChosenSize.toString());
+        System.out.println("Stack Empty: " + testWithChosenSize.isEmpty());
+        System.out.println("pop: " + testWithChosenSize.pop());
+        System.out.println(testWithChosenSize.toString());
+        System.out.println("peek: " + testWithChosenSize.peek());
+        System.out.println("Size of array: "+ testWithChosenSize.getSize());
+
+    }
+
+    public static class GenericStack<E>{
+        private E[] array;
+
+        // Default array capacity randomly chosen as 5
+        private final int DEFAULT_CAPACITY = 2;
+
+        // Constructor to construct a stack with the default initial capacity
+        public GenericStack(){
+            array = (E[]) new Object[DEFAULT_CAPACITY];
+        }
+
+        // Constructor to construct a stack with a specified initial capacity
+        public GenericStack(int inputCapacity){
+            array = (E[]) new Object[inputCapacity];
+        }
+
+        public int getSize() {
+            // Finding the first position that is null in the array (the top of the stack):
+            // Loop index variable i
+            int i = 0;
+            // While array at index i is not null, increment i
+            for (i = 0; i < array.length; i++) {
+                if (array[i] == null) {
+                    break;
+                }
+
+            }
+
+            // The last value of i is the location of the top of the stack, i+1 is the size of the stack
+            int stackSize = i;
+            return stackSize;
+        }
+
+        public E peek() {
+            return (E) array[getSize() - 1];
+        }
+
+        public void push(E o) {
+            // Checking the array size
+            if (array[array.length - 1] != null) {
+                // If array is full, create new array that doubles the current array size
+                E[] newArray = (E[]) new Object[array.length * 2];
+
+                // copy the elements form the current array to the new array
+                int i;
+                for (i = 0; i < array.length; i ++) {
+                    newArray[i] = array[i];
+                }
+                // Adding the new element to the end of the new array
+                newArray[i] = o;
+
+                // Redefining array to the size of the new array
+                array = (E[]) new Object[newArray.length];
+
+                // Setting array equal to the new array
+                array = newArray;
+            } else {
+                // else add the new element to the array
+                // getSize() returns the size of the stack which would also be the first position of a null spot in the array
+                array[getSize()] = o;
+            }
+
+
+        }
+
+        public E pop() {
+            E o = array[getSize() - 1];
+            array[getSize() - 1] = null;
+            return o;
+        }
+
+        public boolean isEmpty() {
+            if (array[0] == null) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        @Override
+        public String toString() {
+            // String variable for the array as a string
+            String arrayAsString = "\nStack:";
+            // Adding each element of the array to the string
+            for (int i = 0; i < getSize(); i++) {
+                arrayAsString += ("\n" + array[i]);
+            }
+            return arrayAsString;
+        }
+
+    }
+
 }
