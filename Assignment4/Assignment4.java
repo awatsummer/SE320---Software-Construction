2d2bc34df880d8eacbdab0cf22d4c4c5a8b73c3d
awatsummer <71294412+awatsummer@users.noreply.github.com>
2022-11-12 20:05:58 -0500

    Create Assignment4.java

---
+++ Assignment4/Assignment4.java
@@ -0,0 +1,21 @@
+/*
+ * 
+ * AnnaMaria Summer
+ * SE 320 - Software Construction
+ * Assignment 4
+ * 
+ * 
+ */
+
+import java.util.*;
+public class Assignment4 {
+	
+	public static void main(String args[]) {
+		
+		Question1 q1 = new Question1();
+		Question2 q2 = new Question2();
+		Question3 q3 = new Question3();
+		
+	}
+	
+}
